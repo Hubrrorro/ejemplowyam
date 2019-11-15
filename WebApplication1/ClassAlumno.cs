d6fef3368642fecbc9ef403ad6cb5f308196fa98
eduardo jimenez <eduardo.jimenez@kriosoft.com>
2019-11-15 11:25:04 -0600

    primet

---
+++ WebApplication1/ClassAlumno.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class ClassAlumno
+    {
+        /// <summary>
+        /// Método para crear el alumno
+        /// </summary>
+        /// <param name="nombre">Nombre completo del alumno</param>
+        /// <param name="grado">Grado en el que se encuentra el alumno</param>
+        public void CrearAlumno(string nombre, int grado)
+        {
+            
+        }
+    }
+}
---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// Vista de Ingreso
+        /// </summary>
+        /// <returns>Regresa la vista</returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// Vista de Acerca de 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        /// <summary>
+        /// Vista de contrato
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
